98700036b8108116aad7472532da1c453447d3f4
gourika <gourikasoni.01@gmail.com>
2025-06-25 19:11:13 +0530

    home page ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import TerminalOverlay from "@/components/TerminalOverlay";
 import { Button } from "@/components/ui/button";
+import UserPrograms from "@/components/UserPrograms";
 import { ArrowRightIcon } from "lucide-react";
+
 import Link from "next/link";
 
 const HomePage = () => {
@@ -110,13 +113,52 @@ const HomePage = () => {
             </div>
 
             {/* RIGHT SIDE */}
-            
+            <div className="lg:col-span-5 relative">
 
+              {/* CORNER PIECES */}
+              <div className="absolute -inset-4 pointer-events-none">
+                <div className="absolute top-0 left-0 w-16 h-16 border-l-2 border-t-2 border-border" />
+                <div className="absolute top-0 right-0 w-16 h-16 border-r-2 border-t-2 border-border" />
+                <div className="absolute bottom-0 left-0 w-16 h-16 border-l-2 border-b-2 border-border" />
+                <div className="absolute bottom-0 right-0 w-16 h-16 border-r-2 border-b-2 border-border" />
+              </div>
+              {/* IMAGE CONTANINER */}
+              <div className="relative aspect-square max-w-lg mx-auto">
+                <div  className="relative overflow-hidden rounded-lg bg-cyber-black">
+                <img
+                    src="/Trainityy.png"
+                    alt="AI Fitness Coach"
+                    className="size-full object-cover object-center"
+                  />
+                  
+                  {/*SCANLINES*/}
+                   <div className="absolute inset-0 bg-[linear-gradient(transparent_0%,transparent_calc(50%-1px),var(--cyber-glow-primary)_50%,transparent_calc(50%+1px),transparent_100%)] bg-[length:100%_8px] animate-scanline pointer-events-none" />
+                   {/*DECORATIONS*/}
+                   <div className="absolute inset-0 pointer-events-none">
+                    <div className="absolute top-1/3 left-1/3 w-1/3 h-1/3 border border-primary/40 rounded-full" />
+
+                    {/* Targeting lines */}
+                    <div className="absolute top-1/2 left-0 w-1/4 h-px bg-primary/50" />
+                    <div className="absolute top-1/2 right-0 w-1/4 h-px bg-primary/50" />
+                    <div className="absolute top-0 left-1/2 h-1/4 w-px bg-primary/50" />
+                    <div className="absolute bottom-0 left-1/2 h-1/4 w-px bg-primary/50" />
+                  </div>
+
+                  <div className="absolute inset-0 bg-gradient-to-t from-background via-background/40 to-transparent" />
+                </div>
+
+              </div>
+                {/* TERMINAL OVERLAY */}
+                <div className="absolute top-[110%] left-0 w-full">
+                <TerminalOverlay />
+                </div>
+            </div>
           </div>
         </div>
       </section>
+
+      <UserPrograms />
     </div>
   );
 };
-
 export default HomePage;
